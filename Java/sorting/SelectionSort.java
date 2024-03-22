70fbcff74a9a50a79aa8750aaa4b8ec9cc850acb
daniel-fritsch <157767880+daniel-fritsch@users.noreply.github.com>
2024-03-22 15:29:36 -0400

    Create SelectionSort.java

---
+++ Java/sorting/SelectionSort.java
@@ -0,0 +1,44 @@
+// Selection Sort implementation in java
+
+// Selection sort repeatedly finds the smallest element of the unsorted
+// portion of an array and creates an ordered portion that is returned at the end of the process. 
+
+public class SelectionSort {
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+
+        // Iterate through the array
+        for (int i = 0; i < n - 1; i++) {
+            // Find min 
+            int min_idx = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[min_idx])
+                    min_idx = j;
+            }
+            // place min in correct position
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    // prints out array (used for testing here)
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+    // Sample selection sort implementation: 
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+
+        System.out.println("Original array:");
+        printArray(arr);
+
+        selectionSort(arr);
+
+        System.out.println("Sorted array:");
+        printArray(arr);
+    }
+}
