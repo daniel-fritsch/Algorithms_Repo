db043da6ea8c5dc4831b85756b0ddbdc7dbce7a4
daniel-fritsch <157767880+daniel-fritsch@users.noreply.github.com>
2024-03-29 00:13:31 -0400

    Update check_if_sorted.py

--- Python/stack/check_if_sorted.py
+++ Python/stack/check_if_sorted.py
@@ -1,1 +1,29 @@
 
+
+# Given a stack, a function is_sorted accepts a stack as a parameter and returns true if said stack is sorted. 
+# A stack is considered sorted if it increases from bottom-most element to top-most element. 
+
+
+def is_sorted(stack):
+    stg = []
+  
+    for i in range(len(stack)):
+        if len(stack) == 0:
+            break
+          
+        first_val = stack.pop()
+        if len(stack) == 0:
+            break
+          
+        second_val = stack.pop()
+        if first_val < second_val:
+            return False
+          
+        storage_stack.append(first_val)
+        stack.append(second_val)
+
+    # backup stack saved in stg(storage)
+    for i in range(len(stg)):
+        stack.append(stg.pop())
+
+    return True
